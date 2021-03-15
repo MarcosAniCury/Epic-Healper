a6fd43c3545f0ea7c94ec6006e1fdc65e9c19566
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2021-03-15 17:26:30 -0300

    Teste com Tokens

--- Scripts/TOKENs.py
+++
@@ -1,8 +0,0 @@
-def get_token():
-    return "NzY4ODM1MjM0MDE1NzM5OTA0.X5GPnA.yRxLmRUVka7exIk04xJLsfY1a4g"
-
-def get_tokenCrud():
-    return "mongodb+srv://Miko:meufusca@epichealpertester.tk1nm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-def get_prefix():
-    return "t "
