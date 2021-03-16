69b103cc53825fb58981d010da7185f0f604cb81
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2021-03-15 21:26:02 -0300

    Update TOKENs.py

--- Scripts/TOKENs.py
+++ Scripts/TOKENs.py
@@ -2,7 +2,7 @@ def get_token():
     return "NzY4ODM1MjM0MDE1NzM5OTA0.X5GPnA.yRxLmRUVka7exIk04xJLsfY1a4g"
 
 def get_tokenCrud():
-    return "mongodb+srv://Miko:meufusca@epichealpertester.tk1nm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true"
+    return "mongodb+srv://Miko:meufusca@epichealpertester.tk1nm.mongodb.net/myFirstDatabase?ssl=true&retryWrites=true&w=majority"
 
 def get_prefix():
     return "t "
